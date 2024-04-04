d2b0d235d3431be7159154019ef88c69eea390e5
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-04-04 16:52:50 +0500

    Add: db connection added

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,11 +1,15 @@
 package middleware
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var collection *mongo.Collection
@@ -22,8 +26,28 @@ func loadEnv() {
 	}
 }
 
-func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+func createDbInit() {
+	connectionString := os.Getenv("DB_CONNECTION_STRING")
+	dbName := os.Getenv("DB_NAME")
+	collName := os.Getenv("DB_COLLECTION_NAME")
 
+	clientOptions := options.Client().ApplyURL(connectionString)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	fmt.Println("connected to db")
+
+	collection = client.Database(dbName).Collection(collName)
+	fmt.Println("collection instance created!")
+}
+func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
 }
 
 func CreateTask(w http.ResponseWriter, r *http.Request) {
